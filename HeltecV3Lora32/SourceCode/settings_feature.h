6d18d33de09eb8cc356a67c8f4e928255046cda3
Mark Coultous <65831685+djcasper1975@users.noreply.github.com>
2025-05-27 16:02:44 +0100

    Update settings_feature.h

--- HeltecV3Lora32/SourceCode/settings_feature.h
+++ HeltecV3Lora32/SourceCode/settings_feature.h
@@ -6,9 +6,10 @@ extern String  cfg_ssid;
 extern String  cfg_pass;
 extern float   cfg_freq_mhz;
 extern bool    cfg_duty_override;
+extern uint8_t cfg_channel;
 
 // functions that live in settings_feature.cpp
 void loadConfig();
 void saveConfig(const String &ssid, const String &pass,
-                float freq, bool dutyOvrd);
+                float freq, bool dutyOvrd, uint8_t channel);
 void setupSettingsRoutes();
