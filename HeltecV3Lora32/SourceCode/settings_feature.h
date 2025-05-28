07f1822f00eb0546f7867f7e7c0c6a6f609619b3
Mark Coultous <65831685+djcasper1975@users.noreply.github.com>
2025-05-28 19:07:52 +0100

    Update settings_feature.h

--- HeltecV3Lora32/SourceCode/settings_feature.h
+++ HeltecV3Lora32/SourceCode/settings_feature.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <Arduino.h>
+#include <WiFi.h>
 
 // globals that live in settings_feature.cpp
 extern String  cfg_ssid;
@@ -7,9 +8,12 @@ extern String  cfg_pass;
 extern float   cfg_freq_mhz;
 extern bool    cfg_duty_override;
 extern uint8_t cfg_channel;
+extern bool    cfg_lora_enabled;    // LoRa On/Off flag
+extern wifi_power_t cfg_wifi_power; // ESP32 Wi-Fi TX power
+extern uint8_t cfg_lora_power;    // LoRa TX power (0–22 dBm)
 
 // functions that live in settings_feature.cpp
 void loadConfig();
 void saveConfig(const String &ssid, const String &pass,
-                float freq, bool dutyOvrd, uint8_t channel);
+                float freq, bool dutyOvrd, uint8_t channel, bool loraEn, wifi_power_t wifiPower, uint8_t loraPower);
 void setupSettingsRoutes();
