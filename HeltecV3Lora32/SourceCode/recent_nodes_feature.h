9c7a902cd4d5cdfe84c2d47aa603f5eec465578b
Mark Coultous <65831685+djcasper1975@users.noreply.github.com>
2025-06-06 17:45:28 +0100

    Delete HeltecV3Lora32/SourceCode/recent_nodes_feature.h

--- HeltecV3Lora32/SourceCode/recent_nodes_feature.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Arduino.h>
-#include <Preferences.h>
-#include <set>
-
-void  loadRecentNodes();                 // call once from setup()
-void  saveRecentNodes();                 // call when you’ve added one
-void  addRecentNode(const String& id);   // call whenever you see a new node
-const std::set<String>& getRecentNodes();// read-only accessor
